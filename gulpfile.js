d85385971af3e197be03b21f7fbcb19e6850b214
fischjer4 <fischjer4@gmail.com>
2019-05-05 09:10:43 -0600

    added background video and modal

--- gulpfile.js
+++ gulpfile.js
@@ -98,7 +98,7 @@ gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: ''
+      baseDir: './'
     },
    browser: "google chrome"
   })
--- js/solution.js
+++ js/solution.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +16,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -27,7 +27,7 @@
   });
 
   // Collapse the navbar when page is scrolled
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
@@ -36,3 +36,76 @@
   });
 
 })(jQuery); // End of use strict
+
+
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/player_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var tv;
+var vid = { 'videoId': 'a57usBpZMkk', 'suggestedQuality': 'hd720' }
+
+function onYouTubePlayerAPIReady() {
+  tv = new YT.Player('tv', { 
+    videoId: 'a57usBpZMkk',
+    suggestedQuality: 'hd720',
+    events: { 
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    },
+    playerVars: {
+      'autoplay': 1,
+      'controls': 0,
+      'enablejsapi': 1,
+      'showinfo': 0,
+      'rel': 0,
+      'loop': 1,
+      'iv_load_policy': 3,
+      'modestbranding': 1
+    },
+  });
+  videoDialogPlayer = new YT.Player('videoDialogPlayer', {
+    videoId: 'a57usBpZMkk',
+    suggestedQuality: 'hd1080',
+    events: { 
+      'onReady': onPlayerReady2 
+    },
+    playerVars: { 
+      'autoplay': 1, 
+      'controls': 1, 
+      'enablejsapi': 1, 
+      'showinfo': 0, 
+      'rel': 0, 
+      'iv_load_policy': 3,
+      'modestbranding': 1
+
+    },
+  });
+}
+function onPlayerStateChange(e) {
+  if (e.data === YT.PlayerState.ENDED) {
+    tv.playVideo();
+  }
+}
+function onPlayerReady2() {
+  videoDialogPlayer.loadVideoById(vid);
+  videoDialogPlayer.stopVideo();
+}
+
+function onPlayerReady() {
+  tv.loadVideoById(vid);
+  tv.mute();
+}
+
+var videoOverlay = document.getElementById('header-overlay');
+var headerPlayBtn = document.getElementById('play-btn');
+$('#vidoeDialog').on('show.bs.modal', function (e) {
+  videoOverlay.style["opacity"] = 0.7;
+  videoDialogPlayer.playVideo();
+  tv.pauseVideo();
+});
+$('#vidoeDialog').on('hidden.bs.modal', function () {
+  videoOverlay.style["opacity"] = 0.5;
+  videoDialogPlayer.stopVideo();
+  tv.playVideo();
+});
